7fece2d8f33f3a6c3a3ba2707301087061a70d36
Kryptonitea <46747991+Kryptonitea@users.noreply.github.com>
2019-01-16 14:18:02 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,18 +6,33 @@ client.on('ready', () => {
 client.user.setGame(`♥GAMERS♥ | use: $bc `,'https://www.twitch.tv/ℕ g A M Codes♥');
   });
 
-client.on("message", message => {
-        var prefix = "$";
-            if (message.content.startsWith(prefix + "bc")) {  ///الامر
-                         if (!message.member.hasPermission("ADMINISTRATOR"))  return;
-  let args = message.content.split(" ").slice(1);
-  var argresult = args.join(' '); 
-  message.guild.members.filter(m => m.presence.status !== 'all').forEach(m => {
- m.send(`${argresult}\n ${m}`);
-})
- message.channel.send(`\`${message.guild.members.filter(m => m.presence.status !== 'all').size}\` **: عدد الاعضاء المستلمين**`); 
- message.delete(); 
-};     
+client.on('ready', () => {
+  console.log('I am ready!');
+});
+client.on('message',async message => {//Toxic Code
+  if(message.author.bot || message.channel.type === '$bc') return;
+  let args = message.content.split(' ');
+  if(args[0] === `$bc`) {//Toxic Code
+    if(!message.member.hasPermission("MANAGE_GUILD")) return message.channel.send('- **أنت لا تملك الصلاحيات اللازمة لأستخدام هذا الأمر**');
+    if(!args[1]) return message.channel.send('- **يجب عليك كتابة الرسالة بعد الأمر**');
+ 
+    let msgCount = 0;
+    let errorCount = 0;
+    let successCount = 0;
+    message.channel.send(`**- [ :bookmark: :: ${msgCount} ] ・عدد الرسائل المرسلة**\n**- [ :inbox_tray: :: ${successCount} ] ・عدد الرسائل المستلمة**\n**- [ :outbox_tray: :: ${errorCount} ]・عدد الرسائل الغير مستلمة**`).then(msg => {//Toxic Code
+      message.guild.members.forEach(g => {//Toxic Code
+        g.send(args.slice(1).join(' ')).then(() => {//Toxic Code
+          successCount++;
+          msgCount++;
+          msg.edit(`**- [ :bookmark: :: ${msgCount} ] ・عدد الرسائل المرسلة**\n**- [ :inbox_tray: :: ${successCount} ] ・عدد الرسائل المستلمة**\n**- [ :outbox_tray: :: ${errorCount} ]・عدد الرسائل الغير مستلمة**`);
+        }).catch(e => {
+          errorCount++;
+          msgCount++;
+          msg.edit(`**- [ :bookmark: :: ${msgCount} ] ・عدد الرسائل المرسلة**\n**- [ :inbox_tray: :: ${successCount} ] ・عدد الرسائل المستلمة**\n**- [ :outbox_tray: :: ${errorCount} ]・عدد الرسائل الغير مستلمة**`);//Toxic Code
+        });
+      });
+    });
+  }
 });
 
 client.login(process.env.BOT_TOKEN);
